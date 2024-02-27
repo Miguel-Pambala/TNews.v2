8b58ffc27f37fe1a490ed68bd8b8f36ee140edeb
Miguel-Pambala <pambalaofficial@gmail.com>
2024-02-27 21:23:35 +0300

    controladores

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TNews.Models;
+using TNews.Services;
+
+namespace TNews.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriaController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        public IActionResult Index()
+        {
+            var categorias = _categoriaService.ObterTodas();
+            return View(categorias);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var categoria = _categoriaService.ObterPorId(id);
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoriaService.Adicionar(categoria);
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var categoria = _categoriaService.ObterPorId(id);
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoriaService.Atualizar(categoria);
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var categoria = _categoriaService.ObterPorId(id);
+            return View(categoria);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _categoriaService.Excluir(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Controllers/NoticiaController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TNews.Models;
+using TNews.Services;
+
+namespace TNews.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        private readonly INoticiaService _noticiaService;
+
+        public NoticiaController(INoticiaService noticiaService)
+        {
+            _noticiaService = noticiaService;
+        }
+
+        public IActionResult Index()
+        {
+            var noticias = _noticiaService.ObterTodas();
+            return View(noticias);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var noticia = _noticiaService.ObterPorId(id);
+            return View(noticia);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Noticia noticia)
+        {
+            if (ModelState.IsValid)
+            {
+                _noticiaService.Adicionar(noticia);
+                return RedirectToAction("Index");
+            }
+
+            return View(noticia);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var noticia = _noticiaService.ObterPorId(id);
+            return View(noticia);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Noticia noticia)
+        {
+            if (ModelState.IsValid)
+            {
+                _noticiaService.Atualizar(noticia);
+                return RedirectToAction("Index");
+            }
+
+            return View(noticia);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var noticia = _noticiaService.ObterPorId(id);
+            return View(noticia);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _noticiaService.Excluir(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
